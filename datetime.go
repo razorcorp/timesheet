6aed751b9ecbb35dc9ae6e5646beba2db0ec9c13
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2020-03-05 23:59:31 +0000

    fix-worklog-selection: Add work logged dated validation

--- datetime.go
+++ datetime.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"time"
 )
 
@@ -24,4 +25,17 @@ func (app *App) getTime() string {
 
 func (app *App) getTimeFixed() string {
 	return "09:00:00.000+0000"
+}
+
+func (app *App) isDateMatch(datetime string) bool {
+	dateExpr, _ := regexp.Compile("([0-9]{4}-[0-9]{2}-[0-9]{2})")
+	date, err := time.Parse("2006-01-02", dateExpr.FindString(datetime))
+	if err != nil {
+		panic(err)
+	}
+
+	if date.Format("2006-01-02") == time.Now().Format("2006-01-02") {
+		return true
+	}
+	return false
 }
