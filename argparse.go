062136e7415eb5ef80d2f6c57297731cc0c7508a
Praveen Premaratne <praveen.premaratne@groupcdg.com>
2020-03-06 20:18:42 +0000

    Argparser improved

--- argparse.go
+++ argparse.go
@@ -20,7 +20,7 @@ var dateFormat, _ = regexp.Compile("[0-9]{4}-[0-9]{2}-[0-9]{2}")
 func (app *App) Parser() {
 	app.Started = app.getDateTime()
 
-	flag.BoolVar(&app.Help, "h", false, "HELP: Print usage")
+	flag.BoolVar(&app.Help, "h", false, "HELP: This tool can be used to log time spent on a specific Jira ticket on a project.")
 	flag.StringVar(&app.Ticket, "r", "",
 		"REQUIRED: Jira ticket reference. E.g. DDSP-4")
 	flag.StringVar(&app.TimeSpent, "t", "",
@@ -31,7 +31,7 @@ func (app *App) Parser() {
 		"OPTIONAL: A comment about the worklog")
 	flag.StringVar(&app.Encode, "e", "", "HELP: Base64 encode the given credentials."+
 		" Format: email:token;domain. e.g. example@example.com:abcThisIsFake;xyz.atlassian.net")
-	flag.BoolVar(&app.TimeRemaining, "remaining", false, "HELP: Print how many hour can be book for the current day." +
+	flag.BoolVar(&app.TimeRemaining, "remaining", false, "HELP: Print how many hour can be book for the current day."+
 		" -history and -d are also available")
 	flag.BoolVar(&app.History, "history", false, "HELP: Print the timesheet of the day")
 	flag.Parse()
@@ -39,10 +39,14 @@ func (app *App) Parser() {
 }
 
 func (app *App) validate() {
-	if app.Help {
-		fmt.Println("This tool can be used to log time spent on a specific Jira ticket on a project.")
+
+	if len(os.Args[1:]) < 1 {
+		fmt.Printf("no arguments are given\n\n")
 		app.usage()
-		os.Exit(0)
+	}
+
+	if app.Help {
+		app.usage()
 	}
 
 	if app.TimeRemaining {
@@ -74,5 +78,14 @@ func (app *App) validate() {
 }
 
 func (app *App) usage() {
+	fmt.Printf("timesheet (-r -t [-d] [-m]] [[-h] [-e] [-d]) (-remaining [-history])\n")
 	flag.PrintDefaults()
+	fmt.Printf("Example:\n" +
+		"\ttimesheet -r DDSP-XXXX -t 8h -m \"Jenkins pipeline completed\"\n" +
+		"\ttimesheet -r DDSP-XXXX -t 1h -m \"Investigated possible solutions\" -d 2020-03-05\n" +
+		"\ttimesheet -remaining\n" +
+		"\ttimesheet -remaining -d 2020-03-05\n" +
+		"\ttimesheet -remaining -history\n" +
+		"\ttimesheet -remaining -history -d 2020-03-05\n")
+	os.Exit(1)
 }
